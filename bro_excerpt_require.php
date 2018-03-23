6fb5e744e64924a58517aa652ab0c995b2513785
petrozvodsky <petrozavodsky@gmail.com>
2018-03-23 13:32:20 +0300

    add conditions logic

--- bro_excerpt_require.php
+++ bro_excerpt_require.php
@@ -7,7 +7,24 @@ Author URI: https://alkoweb.ru
 Version: 1.0.0
 */
 
-function bro_excerpt_require_update_post($data){
+function bro_excerpt_require_update_post( $data ) {
+
+	if ( empty( wp_strip_all_tags( $data['post_excerpt'], true ) ) ) {
+
+
+		$post_types = apply_filters('bro_excerpt_require_update_post__post_types', ['post']);
+
+		$statuses = apply_filters('bro_excerpt_require_update_post__allow_statuses', [ 'pending', 'pending', 'auto-draft']);
+
+		if( in_array($data['post_type'], $post_types)) {
+
+			if ( ! in_array( $data['post_status'], $statuses ) ) {
+				$data['post_status'] = 'draft';
+			}
+
+		}
+
+	}
 
 	return $data;
 }
