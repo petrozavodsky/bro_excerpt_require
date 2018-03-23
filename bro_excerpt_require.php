9d525ce026902f18613c8f429142fa6382a1b73d
petrozvodsky <petrozavodsky@gmail.com>
2018-03-23 15:32:23 +0300

    add localize

--- bro_excerpt_require.php
+++ bro_excerpt_require.php
@@ -7,6 +7,22 @@ Author URI: https://alkoweb.ru
 Version: 1.0.0
 */
 
+function bro_excerpt_require_admin_notices() {
+	if ( ! isset( $_GET[ 'empty_excerpt' ] ) ) {
+		return;
+	}
+
+	$message = __('Без отрывка запись может быть сохранена только в качестве черновика', 'bro_excerpt_require');
+
+	echo '<div id="notice" class="notice notice-warning is-dismissible"> <p>' . $message . '</p>';
+	echo '<button class="notice-dismiss" type="button">';
+	echo '<span class="screen-reader-text">' . $message . '</span>';
+	echo '</button>';
+	echo '</div>';
+}
+
+add_action( 'admin_notices', 'bro_excerpt_require_admin_notices' );
+
 function bro_excerpt_require_update_post( $ID, $post ) {
 
 	if ( empty( wp_strip_all_tags( $post->post_excerpt, true ) ) ) {
@@ -19,16 +35,16 @@ function bro_excerpt_require_update_post( $ID, $post ) {
 		if ( in_array( $post->post_type, $post_types ) ) {
 
 			if ( ! in_array( $post->post_status, $statuses ) ) {
-				remove_action( 'save_post', 'bro_excerpt_require_update_post' );
+				remove_action( 'save_post', 'bro_excerpt_require_update_post',8 );
 
 
 				wp_update_post( [ 'ID' => $ID, 'post_status' => 'draft' ] );
 
-				add_filter( 'redirect_post_location', function ( $location ) {
-					return add_query_arg( [ 'empty_excerpt' => 'ID' ], $location );
+				add_filter( 'redirect_post_location', function ( $location ) use ( $ID ) {
+					return add_query_arg( [ 'empty_excerpt' => $ID ], $location );
 				}, 99 );
 
-				add_action( 'save_post', 'bro_excerpt_require_update_post' );
+				add_action( 'save_post', 'bro_excerpt_require_update_post',8 );
 			}
 
 		}
@@ -37,29 +53,11 @@ function bro_excerpt_require_update_post( $ID, $post ) {
 
 }
 
-add_action( 'save_post', 'bro_excerpt_require_update_post', 10, 2 );
-
-function bro_excerpt_require_admin_notices() {
-	if ( ! isset( $_GET[ 'empty_excerpt' ] ) ) {
-		return;
-	}
-
-	$message = __('Без отрывка запись может быть сохранена в качестве черновика', 'bro_excerpt_require');
-
-
-	echo '<div id="notice" class="notice notice-warning is-dismissible"> <p>' . $message . '</p>';
-	echo '<button class="notice-dismiss" type="button">';
-	echo '<span class="screen-reader-text">' . $message . '</span>';
-	echo '</button>';
-	echo '</div>';
-}
-
-add_action( 'admin_notices', 'bro_excerpt_require_admin_notices' );
-
+add_action( 'save_post', 'bro_excerpt_require_update_post', 8, 2 );
 
 
 function bro_excerpt_require_load_textdomain(){
-	$mo_file_path = dirname(__FILE__) . '/lang/'. get_locale() . '.mo';
+	$mo_file_path = dirname(__FILE__) . '/lang/bro_excerpt_require-'. get_locale() . '.mo';
 	load_textdomain('bro_excerpt_require', $mo_file_path );
 }
 
