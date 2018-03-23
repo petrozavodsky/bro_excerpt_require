bdcb7ce1d3a2999c4f557a5cc6081e0771087c3a
petrozvodsky <petrozavodsky@gmail.com>
2018-03-23 14:42:55 +0300

    add notice

--- bro_excerpt_require.php
+++ bro_excerpt_require.php
@@ -7,28 +7,51 @@ Author URI: https://alkoweb.ru
 Version: 1.0.0
 */
 
-function bro_excerpt_require_update_post( $data ) {
+function bro_excerpt_require_update_post( $ID, $post, $update ) {
 
-	if ( empty( wp_strip_all_tags( $data['post_excerpt'], true ) ) ) {
+	if ( empty( wp_strip_all_tags( $post['post_excerpt'], true ) ) ) {
 
 
-		$post_types = apply_filters('bro_excerpt_require_update_post__post_types', ['post']);
+		$post_types = apply_filters( 'bro_excerpt_require_update_post__post_types', [ 'post' ] );
 
-		$statuses = apply_filters('bro_excerpt_require_update_post__allow_statuses', [ 'pending', 'pending', 'auto-draft']);
+		$statuses = apply_filters( 'bro_excerpt_require_update_post__allow_statuses', [ 'pending', 'pending', 'auto-draft' ] );
 
-		if( in_array($data['post_type'], $post_types)) {
+		if ( in_array( $post['post_type'], $post_types ) ) {
 
-			if ( ! in_array( $data['post_status'], $statuses ) ) {
-				$data['post_status'] = 'draft';
+			if ( ! in_array( $post['post_status'], $statuses ) ) {
+				remove_action( 'save_post', 'bro_excerpt_require_update_post' );
+
+
+				wp_update_post( [ 'ID' => $ID, 'post_status' => 'draft' ] );
+
+				add_filter( 'redirect_post_location', function ( $location ) {
+					return add_query_arg( [ 'empty_excerpt' => 'ID' ], $location );
+				}, 99 );
+
+				add_action( 'save_post', 'bro_excerpt_require_update_post' );
 			}
 
 		}
 
 	}
 
-	return $data;
 }
 
+add_action( 'save_post', 'bro_excerpt_require_update_post', 10, 2 );
 
-add_action( 'wp_insert_post_data', 'bro_excerpt_require_update_post', 1, 1 );
+function bro_excerpt_require_admin_notices() {
+	if ( ! isset( $_GET[ $this->query_param ] ) ) {
+		return;
+	}
 
+	$message = "Без отрывка запись может быть только черновиком";
+
+
+	echo '<div id="notice" class="notice notice-warning is-dismissible"> <p>' . $message . '</p>';
+	echo '<button class="notice-dismiss" type="button">';
+	echo '<span class="screen-reader-text">' . $message . '</span>';
+	echo '</button>';
+	echo '</div>';
+}
+
+add_action( 'admin_notices', 'bro_excerpt_require_admin_notices' );
